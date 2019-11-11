4797f14217ce44eecdfba255464fb6e03bb038b3
Tamas Gal <tgal@km3net.de>
2019-11-11 17:31:40 +0100

    Save less often

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ window.onload = function() {
 
     var fps = 60.;
     setInterval(update, 1000/fps);
-    setInterval(saveState, 1000/fps);
+    setInterval(saveState, 1000);
 }
 
 function restoreState() {
