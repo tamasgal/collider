4e67d716d00e8c99179a5d32b052a740bf6057b6
Tamas Gal <tgal@km3net.de>
2019-11-11 17:19:56 +0100

    Rename restore button

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ function reset() {
 
 function initGUI() {
     GUI.buttons.push(new Button("NEW ROUND", 50, 50, 100, 20, clickNewRound));
-    GUI.buttons.push(new Button("RESTORE", G.world.width-50-100, 50, 100, 20, reset));
+    GUI.buttons.push(new Button("RESET", G.world.width-50-100, 50, 100, 20, reset));
     GUI.buttons.push(
         new UpgradeButton("MULTIPLIER", 50, 100, 220, 20,
             displayValue = function() {
