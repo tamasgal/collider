9f9b0761e966cd258c230717c48f9bc2e054a702
Tamas Gal <tgal@km3net.de>
2019-11-11 17:02:38 +0100

    Fix cookie

--- main.js
+++ main.js
@@ -91,12 +91,36 @@ function saveState() {
     setCookie("G", JSON.stringify(G), 365);
 }
 
-function setCookie(cookiename, cookievalue, days) {
-    var date = new Date();
-    date.setTime(date.getTime() + Number(days) * 36 * 3600 * 1000);
-    document.cookie = cookiename + "=" + cookievalue + "; path=/;expires = " + date.toGMTString();
-
+function setCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
+
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+
 
 function initialise() {
     window.addEventListener('resize', resizeCanvas, false);
